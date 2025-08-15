06efb2a63d3df1995f7ea36dbd3907d5332508dc
Gloria <gloria.boycheva@gmail.comgit statusgit config --global user.email gloria.boycheva@gmail.com>
2025-08-16 01:01:02 +0300

    Add Foody files

---
+++ Floody/Models/NewFolder/FoodDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Floody.Models.NewFolder
+{
+    internal class FoodDTO
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+      
+    }
+}
